4002b2f07378496ea6657a711137feea8a3dd4b5
onejati <omidnejati.it@gmail.com>
2017-03-30 10:18:40 -0700

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -100,10 +100,7 @@ resource "aws_eip" "main" {
 }                                                                                                   
 resource "aws_nat_gateway" "main" {                
     allocation_id   = "${aws_eip.main.id}"             
-    subnet_id       = "${aws_subnet.WWW_a.id}" 
-    tags {
-        Name        = "{var.tag_name}-nat"
-    }	
+    subnet_id       = "${aws_subnet.WWW_a.id}" 	
 }
 
 #---------------------VGW----------------------#
